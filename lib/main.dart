5458ef95b68314283f4d6b94c38cf94b07eda940
jo-dev-pro <tsoyfb88@gmail.com>
2026-07-02 17:07:44 +0900

    111

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'common/route/app_routers.dart';
 
